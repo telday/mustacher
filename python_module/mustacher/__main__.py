5f53d3f2c4a98fb6d2aa592d68e12474d2002de1
telday <ejw393@gmail.com>
2021-12-09 14:06:47 -0500

    Updated python module to be more user friendly

--- python_module/mustacher/__main__.py
+++ python_module/mustacher/__main__.py
@@ -1,6 +1,6 @@
 import cv2
 import sys
-from .mustacher import mustache_image
+from .mustacher import _mustache_image
 from . import resources as mustaches
 import importlib.resources as resources
 
@@ -12,7 +12,7 @@ image = cv2.imread(imagePath, cv2.IMREAD_UNCHANGED)
 with resources.path(mustaches, 'mustache.jpg') as mustache_file:
     mustache = cv2.imread(str(mustache_file), cv2.IMREAD_UNCHANGED)
 
-mustached = mustache_image(image, mustache)
+mustached = _mustache_image(image, mustache)
 
 cv2.imwrite('out.jpeg', mustached)
 cv2.imshow("Faces found", mustached)
--- python_module/mustacher/mustacher.py
+++ python_module/mustacher/mustacher.py
@@ -3,56 +3,69 @@ import sys
 import pathlib
 import importlib.resources as resources
 
+from . import resources as mustaches
 from . import cascades
 
-def get_cascade_classifier():
-    pass
+def mustache_image(image_file, output_filename=None):
+    with resources.path(mustaches, 'mustache.jpg') as mustache_file:
+        mustache = cv2.imread(str(mustache_file), cv2.IMREAD_COLOR)
 
-def mustache_image(image, mustache):
-    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    image = cv2.imread(image_file, cv2.IMREAD_UNCHANGED)
 
-    #cascade_sheet_file = pathlib.Path(str(cascades.__path__)) / 'cascades.xml'
+    image_mustachified = _mustache_image(image, mustache)
+
+    if output_filename is not None:
+        cv2.imwrite(output_filename, image_mustachified)
+
+    return image_mustachified
+
+def _mustache_image(image, mustache):
+    grayscale_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+    faces = detect_faces(grayscale_image)
+
+    for face in faces:
+        image = put_mustache_on_face(image, face, mustache)
+
+    return image
+
+def put_mustache_on_face(image, face, mustache):
+    """ """
+    face_cols, face_rows = (face[2], face[3])
+
+    # Resize the mustache in proportion to the face size
+    new_mustache_size = (int(face_cols / 2), int(face_rows / 2))
+    mustache = cv2.resize(mustache, new_mustache_size, cv2.INTER_AREA)
+    mustache_h, mustache_w = (mustache.shape[0], mustache.shape[1])
+
+    # Find the center of the mustache overlay
+    mustache_coords_center = (face[1] + int(.69 * face[3]), face[0] + int(.5 * face[2]))
+    must_c, must_r = (
+        mustache_coords_center[0] - int(.5 * mustache_h),
+        mustache_coords_center[1] - int(0.5 * mustache_h)
+    )
+
+    image_matrix = image[must_c:must_c + mustache_w, must_r:must_r + mustache_h]
+
+    for col in range(len(mustache)):
+        for row in range(len(mustache[col])):
+            if sum(mustache[col][row]) <= 150:
+                image_matrix[col][row] = mustache[col][row]
+
+    overlay = image_matrix
+
+    image[must_c:must_c + mustache_w, must_r:must_r + mustache_h] = overlay
+
+    return image
+
+def detect_faces(single_layer_image):
+    """Detect faces in an image, the image must be a single layer (greyscale usually)"""
     with resources.path(cascades, "cascade.xml") as cascade_sheet_file:
         cascade_sheet = cv2.CascadeClassifier(str(cascade_sheet_file))
 
-    # detect faces in the image
-    faces = cascade_sheet.detectMultiScale(
-        gray,
-        scaleFactor=1.1,
-        minNeighbors=5,
-        minSize=(30, 30),
-        #    flags = cv2.cv.CV_HAAR_SCALE_IMAGE
+    return cascade_sheet.detectMultiScale(
+            single_layer_image,
+            scaleFactor=1.1,
+            minNeighbors=5,
+            minSize=(30, 30),
         )
-
-    '''
-    for x, y, w, h in faces:
-        cv2.rectangle(image, (x, y), (x+w, y+h), (255, 0, 0), 2)
-    '''
-
-    for face in faces:
-        face_cols, face_rows = (face[2], face[3])
-
-        # Resize the mustache in proportion to the face size
-        new_mustache_size = (int(face_cols / 2), int(face_rows / 2))
-        mustache = cv2.resize(mustache, new_mustache_size, cv2.INTER_AREA)
-        mustache_h, mustache_w = (mustache.shape[0], mustache.shape[1])
-
-        # Find the center of the mustache overlay
-        mustache_coords_center = (face[1] + int(.69 * face[3]), face[0] + int(.5 * face[2]))
-        must_c, must_r = (
-            mustache_coords_center[0] - int(.5 * mustache_h),
-            mustache_coords_center[1] - int(0.5 * mustache_h)
-        )
-
-        image_matrix = image[must_c:must_c + mustache_w, must_r:must_r + mustache_h]
-
-        for col in range(len(mustache)):
-            for row in range(len(mustache[col])):
-                if sum(mustache[col][row]) <= 80:
-                    image_matrix[col][row] = mustache[col][row]
-
-        overlay = image_matrix
-
-        image[must_c:must_c + mustache_w, must_r:must_r + mustache_h] = overlay
-
-    return image
